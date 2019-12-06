55f5fc493ca14a506d74dfd85aedd3f907c21342
chriswodz <58484685+chriswodz@users.noreply.github.com>
2019-12-06 17:48:27 +0000

    Update Shop Drawing Renaming Script.py

--- Shop Drawing Renaming Script.py
+++ Shop Drawing Renaming Script.py
@@ -6,7 +6,15 @@ input_path = r'C:\Users\Chris.Wodzinski\Desktop\test\\'
 output_path = r'C:\Users\Chris.Wodzinski\Desktop\test\output\\'
 
 df = pd.read_csv('test.csv')
-    
+  
+
+
+#DEFINE LEVEL 
+L = 16 
+
+
+
+  
 for i in df.index: 
     os.rename(input_path + '{}.pdf'.format(df['Name'].iloc[i]),
-              output_path + 'C03-KEL-01A-13-DRG-ST_{}_{}_{}_rev{}.pdf'.format(df['Name'].iloc[i], df['Iss'].iloc[i], df['Rev'].iloc[i], df['Status'].iloc[i]))
+              output_path + 'C03-KEL-01A-'+str(L)+'-DRG-ST-{}_iss{}_rev{} RBG STATUS {}.pdf'.format(df['Name'].iloc[i], df['Iss'].iloc[i], df['Rev'].iloc[i], df['Status'].iloc[i]))
